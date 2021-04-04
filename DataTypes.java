84a11e2b5a48c49bfa0a2c8ad6e0299e4968545e
abhinay <63472859+abhinay-developer@users.noreply.github.com>
2021-04-04 21:33:22 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+class DataTypes{
+
+
+public static void main(String args[]){
+  int     age=25;
+  long    mobileNumber=9912177889l;
+  double  salary=12200020002.14;
+  float   temperature=35.4f;
+  String  name="venkat";
+  char    ch='a';
+  byte    b=12;   
+  short   s=17;
+System.out.println("Age::"+age);
+System.out.println("MobileNumber::"+mobileNumber);
+System.out.println("Salary::"+mobileNumber);
+System.out.println("Temprature::"+temperature);
+System.out.println("Name::"+name);
+System.out.println("Char Value::"+ch);
+System.out.println("Byte Value::"+b);
+System.out.println("Short value::"+s);
+
+
+}
+
+}
